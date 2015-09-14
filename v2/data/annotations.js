d3c2721d0d476e11cad740e56b6a93acf9ddce0c
aminalhazwani <amin.alhazwani@gmail.com>
2015-09-14 12:01:15 +0200

    [add] .gitignore [delete] /v2

--- v2/data/annotations.js
+++
@@ -1,9 +0,0 @@
-var comments = {
-"comments" : [
-	{
-		"el": ".publication_title",
-		"title" : "Publication Title",
-		"comment": "Change the heading tag semantically accordingly to the page structure but do not change the class."
-	}
-]
-};
