16d6c7021ac65d41ef2f88073988f7161c83592c
aminalhazwani <amin.alhazwani@gmail.com>
2015-10-14 17:50:44 +0200

    [style] improve timetable day navigation

--- public/data/annotations.js
+++ public/data/annotations.js
@@ -1,7 +1,7 @@
 var comments = {
 "comments" : [
 	{
-		"el": ".publication_title",
+		"el": ".mediaBlock",
 		"title" : "Publication Title",
 		"comment": "Change the heading tag semantically accordingly to the page structure but do not change the class."
 	}
