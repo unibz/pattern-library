acd9f13177994345a84150c2c1256b8b71914e11
aminalhazwani <amin.alhazwani@gmail.com>
2015-10-14 12:57:23 +0200

    [add] v3 which include timetable and timetable nav

---
+++ v3/data/annotations.js
@@ -0,0 +1,9 @@
+var comments = {
+"comments" : [
+	{
+		"el": ".publication_title",
+		"title" : "Publication Title",
+		"comment": "Change the heading tag semantically accordingly to the page structure but do not change the class."
+	}
+]
+};
