eb68128582b8701d979db4f898789c3f90e67fd7
aminalhazwani <amin.alhazwani@gmail.com>
2015-10-22 11:41:12 +0200

    [add] v4

---
+++ v4/data/annotations.js
@@ -0,0 +1,9 @@
+var comments = {
+"comments" : [
+	{
+		"el": ".mediaBlock",
+		"title" : "Publication Title",
+		"comment": "Change the heading tag semantically accordingly to the page structure but do not change the class."
+	}
+]
+};
