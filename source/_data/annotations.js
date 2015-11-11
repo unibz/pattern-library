ce06dcaf0de246d34606f466b2579689c12d30b1
aminalhazwani <amin.alhazwani@gmail.com>
2015-11-11 12:20:00 +0100

    [style] minor changes

--- source/_data/annotations.js
+++ source/_data/annotations.js
@@ -1,9 +1,5 @@
 var comments = {
 "comments" : [
-	{
-		"el": ".mediaBlock",
-		"title" : "Publication Title",
-		"comment": "Change the heading tag semantically accordingly to the page structure but do not change the class."
-	}
+
 ]
 };
